9a989dc99118f86b1d007a1b9b1142b90207dd9a
Your Name <you@example.com>
2025-04-29 21:58:48 +0200

    ajout de site scapping

--- ms_scraper/scrapper/scraping_methods.py
+++ ms_scraper/scrapper/scraping_methods.py
@@ -19,6 +19,80 @@ def scraper_lejournalducoin(url):
         print(link)
     return links
 
+def scraper_cointelegraph(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Erreur HTTP {response.status_code}")
+        return []
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = []
+
+    for a_tag in soup.find_all('a', class_='post-card-inline__title-link'):
+        href = a_tag.get('href')
+        if href and href.startswith("https://cointelegraph.com"):
+            links.append(href)
+
+    return list(set(links))
+
+def scraper_cryptoast(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Erreur HTTP {response.status_code}")
+        return []
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = []
+
+    for a_tag in soup.find_all('a', class_='post-title'):
+        href = a_tag.get('href')
+        if href and href.startswith("https://cryptoast.fr"):
+            links.append(href)
+
+    return list(set(links))
+
+def scraper_decrypt(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Erreur HTTP {response.status_code}")
+        return []
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = []
+
+    for a_tag in soup.find_all('a', class_='article-card-title-link'):
+        href = a_tag.get('href')
+        if href and href.startswith("https://decrypt.co"):
+            links.append(href)
+
+    return list(set(links))
+
+def scraper_blockworks(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Erreur HTTP {response.status_code}")
+        return []
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = []
+
+    for a_tag in soup.find_all('a', href=True):
+        href = a_tag['href']
+        if href.startswith("/news/"):
+            full_url = "https://blockworks.co" + href
+            links.append(full_url)
+
+    return list(set(links))
+
+
 # url = "https://journalducoin.com/"
 # articles = scraper_lejournalducoin(url)
 # for article in articles:
